0016dd2f887c418650a1f014519778ec17a10818
akshit913 <akshit.9@gmail.com>
2020-06-12 21:09:23 -0400

    JS-2 complete

--- functions.js
+++ functions.js
@@ -26,3 +26,20 @@ function createGreeter (greeting) {
 let g1 = createGreeter('Good Morning')
 let g2 = createGreeter('Good Evening')
 console.log(g1('akshit'))
+
+class FruitClass {
+    constructor(taste,color) {
+        this.color = color;
+        this.taste = taste;
+    }
+}
+
+var f = new FruitClass("kiwi","sour");
+console.log("f is",f);
+let mainstr = "hi this is akshit mehta"
+let small= mainstr.slice(1,6);
+console.log(small)
+let subs = mainstr.substring(1,6)
+console.log(subs)
+let smalln= mainstr.slice(-3,-1)
+console.log(smalln)
