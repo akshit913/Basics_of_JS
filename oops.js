65e04bccd946c82148ebd7320fcb63178740ee03
akshit913 <akshit.9@gmail.com>
2020-06-20 13:17:01 -0400

    OOPS

---
+++ oops.js
@@ -0,0 +1,9 @@
+console.log('typeof String',typeof String)
+console.log('typeof Boolean',typeof Boolean)
+console.log('typeof Number',typeof Number)
+console.log('typeof Array',typeof Array)
+console.log('typeof Object',typeof Object)
+console.log('typeof Function',typeof Function)
+let obj = {a:2, b:'akshit'}
+console.log('should be true',obj.__proto__ == Object.prototype)
+console.log(Number.prototype)
